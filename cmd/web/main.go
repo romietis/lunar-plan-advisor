fb2578f52437fdecc3d1987790100d8680205358
ROKV (Romans Kotikovs) <ROKV@novonordisk.com>
2024-11-26 21:11:44 +0100

    interest rate update

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,10 +12,10 @@ import (
 func main() {
 	planConfig := advisor.Plans{
 		Plans: []advisor.Plan{
-			{Name: "Light", AnnualInterestRate: 1.25, Fee: 0.0, Cap: 100000},
-			{Name: "Standard", AnnualInterestRate: 1.5, Fee: 29.0, Cap: 100000},
-			{Name: "Plus", AnnualInterestRate: 1.75, Fee: 69.0, Cap: 0},
-			{Name: "Unlimited", AnnualInterestRate: 2.25, Fee: 139.0, Cap: 0},
+			{Name: "Light", AnnualInterestRate: 0.75, Fee: 0.0, Cap: 100000},
+			{Name: "Standard", AnnualInterestRate: 1.0, Fee: 29.0, Cap: 100000},
+			{Name: "Plus", AnnualInterestRate: 1.25, Fee: 69.0, Cap: 0},
+			{Name: "Unlimited", AnnualInterestRate: 1.75, Fee: 139.0, Cap: 0},
 		},
 	}
 
